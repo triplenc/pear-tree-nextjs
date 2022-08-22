7fbc063d37bfd9d20d629bac5f4976bdaa36234d
Jokyuseong <pandora_h@naver.com>
2022-08-22 23:00:44 +0900

    ADD | theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '@styles/globals.css'
+import {theme} from '@styles/theme'
 import type {AppProps} from 'next/app'
 import {RecoilRoot} from 'recoil'
+import {ThemeProvider} from 'styled-components'
 
-function MyApp({Component, pageProps}: AppProps): JSX.Element {
+function App({Component, pageProps}: AppProps): JSX.Element {
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </RecoilRoot>
   )
 }
 
-export default MyApp
+export default App
