4a4bfce4f1c92f37bc6bfcbbaeb964ed35deff6f
Jokyuseong <pandora_h@naver.com>
2022-08-27 11:33:39 +0900

    UPDATE | stitches utils

--- src/components/stitches.config.ts
+++ src/components/stitches.config.ts
@@ -46,9 +46,45 @@ export const {
     fontWeights,
   },
   utils: {
-    marginX: (value: PropertyValue<"margin">) => ({
+    mt: (value: PropertyValue<"margin">) => ({
+      marginTop: value,
+    }),
+    mr: (value: PropertyValue<"margin">) => ({
+      marginRight: value,
+    }),
+    ml: (value: PropertyValue<"margin">) => ({
+      marginLeft: value,
+    }),
+    mb: (value: PropertyValue<"margin">) => ({
+      marginBottom: value,
+    }),
+    mx: (value: PropertyValue<"margin">) => ({
       marginLeft: value,
       marginRight: value,
+    }),
+    my: (value: PropertyValue<"margin">) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+    pt: (value: PropertyValue<"padding">) => ({
+      paddingTop: value,
+    }),
+    pr: (value: PropertyValue<"padding">) => ({
+      paddingRight: value,
+    }),
+    pl: (value: PropertyValue<"padding">) => ({
+      paddingLeft: value,
+    }),
+    pb: (value: PropertyValue<"padding">) => ({
+      paddingBottom: value,
+    }),
+    px: (value: PropertyValue<"padding">) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: PropertyValue<"padding">) => ({
+      paddingTop: value,
+      paddingBottom: value,
     }),
   },
 });
