3b43da88830b1b08de6fb87a6677c745e1139d9e
Jokyuseong <pandora_h@naver.com>
2022-09-09 17:54:16 +0900

    UPDATE | prettier

--- src/recoil/party/atom.ts
+++ src/recoil/party/atom.ts
@@ -1,22 +1,22 @@
-import {atom} from 'recoil'
+import { atom } from "recoil";
 
 export const partyAtom = atom({
-  key: 'partyAtom',
+  key: "partyAtom",
   default: [
     {
-      name: 'party1',
+      name: "party1",
       active: true,
       priority: 1,
     },
   ],
-})
+});
 
 export const partyFilterStateAtom = atom({
-  key: 'partyFilterStateAtom',
-  default: 'all',
-})
+  key: "partyFilterStateAtom",
+  default: "all",
+});
 
 export const partyPriorityStateAtom = atom({
-  key: 'partyPriorityStateAtom',
-  default: 'DESC',
-})
+  key: "partyPriorityStateAtom",
+  default: "DESC",
+});
