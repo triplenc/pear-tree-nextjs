7934e672b96cdc2ab2755310b4d79e9d53049b46
Jokyuseong <pandora_h@naver.com>
2022-09-10 16:29:11 +0900

    feat(_document): ssr stitches 적용, meta tag 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,22 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
+import { getCssText } from "@components/stitches.config";
+
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ko">
       <Head>
         <meta name="description" content="배달료 쉐어 서비스" />
-        <link rel="icon" href="/favicon.png" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
       </Head>
       <body>
         <Main />
