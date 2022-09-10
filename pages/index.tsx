f85f47728bebaa31b47763d436f7b08cb5dfdf8a
Jokyuseong <pandora_h@naver.com>
2022-09-10 17:29:24 +0900

    fix: title meta tag index -> _app 이동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 
+import { Button } from "@components/common";
 import { colors } from "@components/stitches.config";
 import Chicken from "@icons/chicken.svg";
 import Star from "@icons/star.svg";
@@ -8,11 +8,10 @@ import Star from "@icons/star.svg";
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>배나무</title>
-      </Head>
       <button onClick={() => console.info(1234)}>
-        <Chicken />
+        <Button>
+          <Chicken />
+        </Button>
         <Star fill={colors.main01} />
       </button>
     </div>
