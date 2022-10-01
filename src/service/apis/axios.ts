96866c093f216973e0345816cc54f5ba06b1e571
Jokyuseong <pandora_h@naver.com>
2022-10-01 22:07:34 +0900

    feat: apis 삭제 api 추가

--- src/service/apis/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: process.env.API_URL,
-  headers: { "X-Custom-Header": "foobar" },
-  timeout: 1000,
-});
-
-// TODO: axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
