8c2c98747aba061b0454c97a29c5221f0f7182c6
dongtony12 <dongtony0523@naver.com>
2022-11-03 07:35:12 +0900

    feat: next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [],
   },
+  reactStrictMode: true,
+  swcMinify: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
